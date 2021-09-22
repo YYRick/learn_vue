248abf55d0490669246c76295f3879b5f289c10f
YYRick <2979434806@qq.com>
2021-09-22 23:29:56 +0800

    ...

---
+++ 12_前端模块化/01_为什么需要有模块化/aaa.js
@@ -0,0 +1,13 @@
+//小明
+var name = '小明';
+var age = 22;
+
+function sum(num1, num2){
+    return num1 + num2;
+}
+
+var flag = true;
+
+if(flag){
+    console.log(sum(10, 20));
+}
---
+++ 12_前端模块化/01_为什么需要有模块化/mmm.js
@@ -0,0 +1,3 @@
+if(flag){
+    console.log('小明是天才，哈哈哈');
+}
