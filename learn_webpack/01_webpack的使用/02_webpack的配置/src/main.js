0a72c6004ba256783298cc277f9843fb97f92bd2
YYRick <2979434806@qq.com>
2021-09-23 20:57:47 +0800

    webpack基本配置

---
+++ learn_webpack/01_webpack的使用/02_webpack的配置/src/main.js
@@ -0,0 +1,9 @@
+const {add, mul} = require('./mathUtils.js')
+
+console.log(add(20, 30));
+console.log(mul(20, 30));
+
+import {name, age, height} from './info.js'
+console.log(name);
+console.log(age);
+console.log(height);
---
+++ learn_webpack/01_webpack的使用/02_webpack的配置/src/mathUtils.js
@@ -0,0 +1,13 @@
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function mul(num1, num2){
+    return num1 * num2;
+}
+
+//Common.js
+module.exports = {
+    add,
+    mul
+}
