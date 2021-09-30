d4bb77600306fc9a3defead0f608e8bd9e09ee99
YYRick <2979434806@qq.com>
2021-09-30 20:22:03 +0800

    ...

---
+++ learn_axios/src/main.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
+
+
+axios({
+  url: 'http://123.207.32.32:8000/home/multidata',
+  method: 'get'
+}).then(res => {
+  console.log(res);
+})
